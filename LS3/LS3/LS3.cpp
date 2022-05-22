5aa21a906253a1e82d10bda9027a96d22890a22d
Vasiliy_SH <shatov2702@yandex.ru>
2022-05-22 21:50:53 +0300

    + задание 2

--- LS3/LS3/LS3.cpp
+++ LS3/LS3/LS3.cpp
@@ -3,13 +3,24 @@
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");   // Руский язык
+    setlocale(LC_ALL, "Russian");   // Русский язык
 
     // задание 1,4:
-    extern const int a;     // Считываем const переменные из другова .срр файла
+    std::cout << "Задание 1,4:" << std::endl;
+    extern const int a;     // Считываем const переменные из другого .срр файла
     extern const int b;
     extern const int c;
     extern const int d;
     std::cout << "Решение через static_cast <float> ():   " << a * (b + (static_cast <float>(c) / d)) << std::endl;
-    printf("Решение через float ():\t\t\t%2.3f\n", a * (b + (float(c) / d)));
+    printf("Решение через float ():\t\t\t%2.3f\n\n", a * (b + (float(c) / d)));
+
+    // задение 2:
+    std::cout << "Задание 2:" << std::endl;
+    int num = 19;   // Число для сравнения (разница = 2)
+    (num <= 21) ? std::cout << "Число меньше, разница = " << 21 - num << std::endl :        // Если число меньше 21 - выводиться разница 
+        std::cout << "Число больше, двойная разница = " << 2 * (num - 21) << std::endl;     // Если число меньше 21 - выводиться двойная разница        
+    num += 5;       // num = 24 (разница = 3)
+    (num <= 21) ? std::cout << "Число меньше, разница = " << 21 - num << std::endl :
+        std::cout << "Число больше, двойная разница = " << 2 * (num - 21) << std::endl;
+
  }
