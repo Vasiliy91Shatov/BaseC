4b4b58f15943ec22b20b14ac19d41e5faa1ac318
Vasiliy_SH <shatov2702@yandex.ru>
2022-05-22 21:24:42 +0300

    + задание 1,4 (+ файл LS3_Source.cpp)

--- LS3/LS3/LS3.cpp
+++ LS3/LS3/LS3.cpp
@@ -1,7 +1,15 @@
 ﻿#include <iostream>
+#include <clocale>
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    setlocale(LC_ALL, "Russian");   // Руский язык
 
+    // задание 1,4:
+    extern const int a;     // Считываем const переменные из другова .срр файла
+    extern const int b;
+    extern const int c;
+    extern const int d;
+    std::cout << "Решение через static_cast <float> ():   " << a * (b + (static_cast <float>(c) / d)) << std::endl;
+    printf("Решение через float ():\t\t\t%2.3f\n", a * (b + (float(c) / d)));
+ }
---
+++ LS3/LS3/LS3_Source.cpp
@@ -0,0 +1,6 @@
+// задание 4: 
+extern const int a = 3;
+extern const int b = 1;
+extern const int c = 5;
+extern const int d = 4;
+// 3*(1+(5/4))=6,75
