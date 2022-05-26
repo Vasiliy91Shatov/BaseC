ebec32400e146e049bfd975abf1740d82f7fd548
Vasiliy_SH <shatov2702@yandex.ru>
2022-05-26 21:05:06 +0300

    + Задание 4

--- LS4/LS4/LS4.cpp
+++ LS4/LS4/LS4.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
 #include <clocale>
 
+// Задание 4 функция:
+void PrNum(int num)				// функция определяющая являеться ли число простым, и выводящая на экран соответствующую строку
+{
+	int n = 0;					// счетчик количества делений числа
+	for (int i = 1; i <= num; i++)
+	{
+		if (n == 3)
+			break;				// прерываем цикл, если n = 3 (чило уже не является простым, и дальнейшии вычисления не нужны)
+		else if (num % i == 0)	// если число делится на i, то n увеличивается на 1
+			n++;
+	}
+	(n < 3) ? std::cout << "Число является простым" : std::cout << "Число не является простым"; 
+}
 
 int main()
 {
@@ -41,5 +54,14 @@ int main()
 	std::cout << "Нечетные числа от 1 до " << MaxNum << std::endl;
 	for (int i = 1; i <= MaxNum; i += 2)
 		std::cout << i << ", ";
+	std::cout << std::endl << std::endl;
+
+	// Задание 4:
+
+	int num;
+	std::cout << "Задание 4:" << std::endl << std::endl;
+	std::cout << "Введите целое положительное число: ";
+	std::cin >> num;
+	PrNum(num);
 	std::cout << std::endl << std::endl;
 }
